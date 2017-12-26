49611fdbe976c1185f64336917cd19d3d46faabb
wucy <wucy1991@163.com>
2017-12-26 08:07:56 +0800

    add

--- build/run.sh
+++ build/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo scl enable devtoolset-4 bash
 sudo yum -y install centos-release-scl epel-release 
 sudo yum -y install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
 sudo yum -y install git
 sudo yum -y install htop
+sudo yum install -y lrzsz 
 cd /root
-git clone https://github.com/fireice-uk/xmr-stak.git
-mkdir /root/xmr-stak/build 
+git clone https://github.com/qinqinwodetao/xmr-stak.git
 cd /root/xmr-stak/build 
+sudo scl enable devtoolset-4 bash
 cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd /root/xmr-stak/build/bin 
