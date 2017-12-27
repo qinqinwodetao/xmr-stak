2500a2dc6d176b8a44eb6fb621c697c8409825b1
wucy <wuchengyang1991@qq.com>
2017-12-27 14:52:38 +0800

    Update run.sh

--- build/run.sh
+++ build/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 sudo yum -y install centos-release-scl epel-release cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make git htop lrzsz
 cd /root
-git clone https://github.com/qinqinwodetao/xmr-stak.git
-cd /root/xmr-stak/build 
-sudo scl enable devtoolset-4 bash
-cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
-make install
+wget http://ou0iddqf5.bkt.clouddn.com/xmr-stak.tar.gz
+tar -zxvf xmr-stak.tar.gz xmr-stak
 cd /root/xmr-stak/build/bin 
+vi config.txt
 nohup ./xmr-stak &
