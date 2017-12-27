d524d729c7f549abeae8bf1bfc7e16c0793ab096
wucy <wuchengyang1991@qq.com>
2017-12-27 15:08:51 +0800

    Update run.sh

--- build/run.sh
+++ build/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+PUBLIC_IP=`wget http://ipecho.net/plain -O - -q ; echo`
+
 sudo yum -y install centos-release-scl epel-release cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make git htop lrzsz
 cd /root
 wget http://ou0iddqf5.bkt.clouddn.com/xmr-stak.tar.gz
 tar -zxvf xmr-stak.tar.gz xmr-stak
 cd /root/xmr-stak/build/bin 
-vi config.txt
+sed -i 's/T9/$PUBLIC_IP/g' config.txt
 nohup ./xmr-stak &
